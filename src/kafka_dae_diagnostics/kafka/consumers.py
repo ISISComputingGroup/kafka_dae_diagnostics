294ef0c7476d8603e531d51a01ad083da4e9413e
Jack Harper <jack.harper@stfc.ac.uk>
2026-08-24 13:30:53 +0100

    try to always get most recent values

--- src/kafka_dae_diagnostics/kafka/consumers.py
+++ src/kafka_dae_diagnostics/kafka/consumers.py
@@ -48,8 +48,19 @@ def make_event_consumer(config: DiagnosticsConfig) -> Consumer:
     return event_consumer
 
 def make_vetoconfig_consumer(config :DiagnosticsConfig) -> Consumer:
+    """Make a veto config consumer.
+    
+    This will assign to the topic just before the high watermark so we always
+    have a value (assuming there are some in the topic) for veto configuration.
+    subsequent updates will just overwrite these.
+    """
     vetoconfig_consumer = Consumer(config.kafka_vetoconfig_consumer)
-    vetoconfig_consumer.assign([TopicPartition(config.vetoconfig_topic, 0)])
+    low, high = vetoconfig_consumer.get_watermark_offsets(
+            TopicPartition(config.vetoconfig_topic, 0), cached=False
+        )
+    # assign to just before the last message on the topic so we always have values for veto names
+    start_offset = max(high - 1, low)
+    vetoconfig_consumer.assign([TopicPartition(config.vetoconfig_topic, 0, start_offset)])
     return vetoconfig_consumer
 
 
